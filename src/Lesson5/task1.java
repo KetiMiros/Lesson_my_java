1878336ffa4b3bd539841304a4ed812b6bd31867
Ketrin_Miros <miroslavskayaketi@gmail.com>
2023-05-01 20:02:33 +0300

    Финальное задание урок №6

---
+++ src/Lesson5/task1.java
@@ -0,0 +1,24 @@
+/*Задача 1. Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов. */
+//Вариант решения №1
+package Lesson5;
+import java.util.HashMap;
+import java.util.List;
+public class task1 {
+        public static void main(String[] args) {
+            addInPhoneBook();
+            findInPhoneBook("Иванов");
+        }
+        public static HashMap<String, List<String>> phoneBook = new HashMap<>();
+        public static void addInPhoneBook() {
+            phoneBook.put("Иванов", List.of("+7(111)111-11-11", "+7(222)222-22-22"));
+            phoneBook.put("Петров", List.of("+7(333)333-33-33", "+7(444)444-44-44"));
+            phoneBook.put("Сидоров",List.of("+7(555)555-55-55", "+7(666)666-66-66"));
+
+        }
+
+        public static void findInPhoneBook(String surname) {
+            System.out.printf("%s: %s", surname, phoneBook.get(surname));
+        }
+
+    }
+
